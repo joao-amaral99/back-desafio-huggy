4e0ea8d68a5e4acf16531dea96db8ea869681603
João Amaral <joao.amaral.almeida99@gmail.com>
2025-08-19 15:50:48 -0300

    refactor(auth): clean up auth config to remove user provider and local auth references

--- config/auth.php
+++ config/auth.php
@@ -14,8 +14,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'guard' => env('web'),
+        'passwords' => null
     ],
 
     /*
@@ -38,7 +38,6 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
         ],
     ],
 
@@ -60,11 +59,6 @@ return [
     */
 
     'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
-        ],
-
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
@@ -91,12 +85,7 @@ return [
     */
 
     'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
-            'throttle' => 60,
-        ],
+
     ],
 
     /*
